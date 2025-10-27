248fc622bff9b1761453109d36d97ced0757ff51
tung231195 <thl231195@gmail.com>
2025-10-27 10:34:58 +0700

    fix eslint

--- src/app/api/stripe/webhook/route.js
+++ src/app/api/stripe/webhook/route.js
@@ -1,17 +1,13 @@
 import Stripe from "stripe";
-const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_WEBHOOK_SECRET);
+const stripe = new Stripe(process.env.STRIPE_WEBHOOK_SECRET);
 
 export async function POST(req) {
   const sig = req.headers.get("stripe-signature");
   const body = await req.text(); // raw body
-  console.log("run to web hook", req, process.env.NEXT_PUBLIC_STRIPE_WEBHOOK_SECRET);
+  console.log("run to web hook", req, process.env.STRIPE_WEBHOOK_SECRET);
   let event;
   try {
-    event = stripe.webhooks.constructEvent(
-      body,
-      sig,
-      process.env.NEXT_PUBLIC_STRIPE_WEBHOOK_SECRET,
-    );
+    event = stripe.webhooks.constructEvent(body, sig, process.env.STRIPE_WEBHOOK_SECRET);
   } catch (err) {
     return new Response("Webhook Error" + err, { status: 400 });
   }
